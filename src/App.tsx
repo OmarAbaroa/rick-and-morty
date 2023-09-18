025239cc378f979d2d8ca665d9ee37790c859998
Omar Corchado <omar@timb3r.io>
2023-09-18 14:56:40 -0600

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { CharacterPage, HomePage } from "./views";
 import { Layout } from "./components";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Layout>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/character/:id" element={<CharacterPage />} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
